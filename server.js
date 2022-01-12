22d2f1e005aec3e892e0f4413bf3d525d1ac351d
Sumna Islam <sumanislam1416@gmail.com>
2022-01-12 23:17:42 +0600

    authentication endpoints with middleware

--- server.js
+++ server.js
@@ -10,9 +10,24 @@ const app = express();
 
 app.use(helmet());
 
-app.get('/secret', (req, res) => {
+function checkLoggedIn(req, res, next) {
+  const loggedIn = true; // TODO:
+  if(!loggedIn) {
+    return res.status(401).json({
+      error: 'You must log in',
+    })
+  }
+  next();
+}
+
+app.get('/auth/google', (req, res) => {});
+app.get('/auth/google/callback', (req, res) => {});
+app.get('/auth/logout', (req, res) => {});
+
+app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal secret value is 42!');
-})
+});
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
